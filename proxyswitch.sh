74f183620adcfef1dcd6e087bc3dd6ae55d4ebe5
Roopansh Bansal <roopansh.bansal@gmail.com>
2017-01-22 14:23:24 +0530

    Added NO Proxy and minor UI Improvements.

--- proxyswitch.sh
+++ proxyswitch.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Some global variables
+# Source the Proxy Database
 source "$HOME/.proxyswitch/proxyDB.txt"
-
 
 # Proceed only if root privileges
 CheckRoot(){
@@ -18,39 +17,53 @@ CheckRoot(){
 
 # ask user which proxy he wants to use
 ProxyChoice(){
+	clear
 	echo "You have  $PROXYCOUNT  saved proxies."
 	echo
+
+	echo "0 > USE NO PROXY"
+	echo
+
 	for (( i = 1; i <= $PROXYCOUNT; i++ )); do
 		proxy="${PROXIES[$i-1]}"
+	
 		# display choice number
-		echo -n "$i - "
-		# display username
-		sed 's/\(.*\):.*@\(.*\)/\2 \1/' <<< "$proxy"
+		echo -n "$i > "
+	
+		# display proxy:port and username
+		sed 's/\(.*\):.*@\(.*\)/\2  \1/' <<< "$proxy"
 		echo
 	done
 
-	echo "$i - Some Other Proxy"
+	echo "$i > SOME OTHER PROXY"
 	echo
 
 	read -p "Which proxy you want to use? " ProxyChoice
 	
 	# check if in range
 	if [[ $ProxyChoice -gt '0' && $ProxyChoice -le $PROXYCOUNT ]]; then
-		ProxyChoice=$((ProxyChoice-1))
-		proxy="${PROXIES[$ProxyChoice]}"
+		# ProxyChoice=$((ProxyChoice-1))
+		# Read the proxy details from the database
+		proxy="${PROXIES[$ProxyChoice-1]}"
+		# Set that proxy
 		SetProxy $proxy
-		echo "ProxySwitch Successful."
 	elif [[ $ProxyChoice == $(($PROXYCOUNT + 1)) ]]; then
+		# Set a new Proxy
 		NewProxy
-		echo "ProxySwitch Successful."
+	elif [[ $ProxyChoice == '0' ]]; then
+		# No Proxy
+		ProxyNone
 	else
 		echo "Invalid Proxy Selected."
 		echo "ProxySwitch Failed."
 	fi
-
 }
 
+# Calls functions to set proxy in different fields
 SetProxy(){
+	# Remove all the previous Proxy Settings
+	ProxyNone
+
 	# System Settings Proxy 
 	ProxySYS $1
 	# Apt Proxy Configuration
@@ -59,6 +72,9 @@ SetProxy(){
 	ProxyENV $1
 	# exporting in .bashrc file
 	ProxyBASHRC $1
+
+	echo "New proxy settings applied."
+	echo "ProxySwitch Successful."
 }
 
 # set the System proxy
@@ -73,8 +89,8 @@ ProxySYS(){
 	# echo $ProxyPROXY $ProxyPORT $ProxyUN $ProxyPASS
 	
 	sudo gsettings set org.gnome.system.proxy use-same-proxy true
-	sudo gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8', '::1', '*.iitg.ernet.in', '*.iitg.ac.in', '202.141.*.*', '172.16.*.*']"
 	sudo gsettings set org.gnome.system.proxy mode 'manual'
+	sudo gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8', '::1', '*.iitg.ernet.in', '*.iitg.ac.in', '202.141.*.*', '172.16.*.*']"
 	sudo gsettings set org.gnome.system.proxy.http host "$ProxyPROXY"
 	sudo gsettings set org.gnome.system.proxy.http port "$ProxyPORT"
 	# sudo gsettings set org.gnome.system.proxy.http use-authentication true
@@ -91,7 +107,7 @@ ProxySYS(){
 # set the apt proxy
 ProxyAPT(){
 	proxy="$1"
-	sudo sed -i.bak '/proxy/d' /etc/apt/apt.conf
+	
 	sudo echo -e "Acquire::http::proxy \"http://$proxy/\";\nAcquire::https::proxy \"https://$proxy/\";\nAcquire::ftp::proxy \"ftp://$proxy/\";\nAcquire::socks::proxy \"socks://$proxy/\";" >> /etc/apt/apt.conf
 }
 
@@ -99,12 +115,6 @@ ProxyAPT(){
 ProxyENV(){
 	proxy="$1"
 	
-	#remove previous proxy
-	sudo sed -i.bak '/http_proxy/d' /etc/environment
-	sudo sed -i.bak '/https_proxy/d' /etc/environment
-	sudo sed -i.bak '/ftp_proxy/d' /etc/environment
-	sudo sed -i.bak '/socks_proxy/d' /etc/environment
-
 	sudo echo -e "http_proxy=\"http://$proxy/\"\nhttps_proxy=\"https://$proxy/\"\nftp_proxy=\"ftp://$proxy/\"\nsocks_proxy=\"socks://$proxy/\"" >> /etc/environment
 }
 
@@ -112,18 +122,40 @@ ProxyENV(){
 ProxyBASHRC(){
 	proxy="$1"
 
-	#remove previous proxy
+	sudo echo -e "## Proxy settings by proxyswitch\nexport http_proxy=\"http://$proxy/\"\nexport https_proxy=\"https://$proxy/\"\nexport socks_proxy=\"socks://$proxy/\"\nexport ftp_proxy=\"ftp://$proxy/\"" >> $HOME/.bashrc
+
+	source $HOME/.bashrc
+}
+
+# Set no proxy ... Remove all the previous proxy settings 
+ProxyNone(){
+	# System Settings Proxy 
+	sudo gsettings set org.gnome.system.proxy use-same-proxy true
+	sudo gsettings set org.gnome.system.proxy mode 'none'
+
+	# Apt Proxy Configuration
+	sudo sed -i.bak '/http::proxy/d' /etc/apt/apt.conf
+	sudo sed -i.bak '/https::proxy/d' /etc/apt/apt.conf
+	sudo sed -i.bak '/socks::proxy/d' /etc/apt/apt.conf
+	sudo sed -i.bak '/ftp::proxy/d' /etc/apt/apt.conf
+
+	# Environment variables set up
+	sudo sed -i.bak '/http_proxy/d' /etc/environment
+	sudo sed -i.bak '/https_proxy/d' /etc/environment
+	sudo sed -i.bak '/ftp_proxy/d' /etc/environment
+	sudo sed -i.bak '/socks_proxy/d' /etc/environment
+
+	# exporting in .bashrc file
 	sudo sed -i.bak '/proxyswitch/d' $HOME/.bashrc
 	sudo sed -i.bak '/http_proxy/d' $HOME/.bashrc
 	sudo sed -i.bak '/https_proxy/d' $HOME/.bashrc
 	sudo sed -i.bak '/ftp_proxy/d' $HOME/.bashrc
 	sudo sed -i.bak '/socks_proxy/d' $HOME/.bashrc
 
-	sudo echo -e "## Proxy settings by proxyswitch\nexport http_proxy=\"http://$proxy/\"\nexport https_proxy=\"https://$proxy/\"\nexport socks_proxy=\"socks://$proxy/\"\nexport ftp_proxy=\"ftp://$proxy/\"" >> $HOME/.bashrc
-
-	source $HOME/.bashrc
+	echo "All previous proxy settings removed."
 }
 
+# Set a new proxy not saved in the database 
 NewProxy(){
 	echo
 	echo "Enter Details for proxy : "
