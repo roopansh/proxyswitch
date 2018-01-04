cf4c6942f8f82a62c91d1ca64c4b3b5bfa251a5b
Akshat Jain <akshatjain0205@yahoo.co.in>
2018-01-05 00:50:49 +0530

    Added all_proxy variable to prevent SOCKS support

--- proxyswitch.sh
+++ proxyswitch.sh
@@ -95,32 +95,35 @@ ProxySYS(){
 	sudo gsettings set org.gnome.system.proxy.ftp port $ProxyPORT
 	sudo gsettings set org.gnome.system.proxy.socks host $ProxyPROXY
 	sudo gsettings set org.gnome.system.proxy.socks port $ProxyPORT
+	sudo gsettings set org.gnome.system.proxy.all host $ProxyPROXY
+	sudo gsettings set org.gnome.system.proxy.all port $ProxyPORT
 }
 
 # set the apt proxy
 ProxyAPT(){
 	proxy="$1"
 	
-	sudo echo -e "Acquire::http::proxy \"http://$proxy/\";\nAcquire::https::proxy \"https://$proxy/\";\nAcquire::ftp::proxy \"ftp://$proxy/\";\nAcquire::socks::proxy \"socks://$proxy/\";" >> /etc/apt/apt.conf
+	sudo echo -e "Acquire::http::proxy \"http://$proxy/\";\nAcquire::https::proxy \"https://$proxy/\";\nAcquire::ftp::proxy \"ftp://$proxy/\";\nAcquire::socks::proxy \"socks://$proxy/\";\nAcquire::all::proxy \"all://$proxy/\";" >> /etc/apt/apt.conf
 }
 
 # set up the environment variables in the proxy
 ProxyENV(){
 	proxy="$1"
 	
-	sudo echo -e "http_proxy=\"http://$proxy/\"\nhttps_proxy=\"https://$proxy/\"\nftp_proxy=\"ftp://$proxy/\"\nsocks_proxy=\"socks://$proxy/\"" >> /etc/environment
+	sudo echo -e "http_proxy=\"http://$proxy/\"\nhttps_proxy=\"https://$proxy/\"\nftp_proxy=\"ftp://$proxy/\"\nsocks_proxy=\"socks://$proxy/\"\nall_proxy=\"all://$proxy/\"" >> /etc/environment
 
 	export http_proxy="http://$proxy/"
 	export https_proxy="https://$proxy/"
 	export socks_proxy="socks://$proxy/"
 	export ftp_proxy="ftp://$proxy/"
+	export all_proxy="https://$proxy/"
 }
 
 # exporting the variables in the bashrc file.
 ProxyBASHRC(){
 	proxy="$1"
 
-	sudo echo -e "## Proxy settings by proxyswitch\nexport http_proxy=\"http://$proxy/\"\nexport https_proxy=\"https://$proxy/\"\nexport socks_proxy=\"socks://$proxy/\"\nexport ftp_proxy=\"ftp://$proxy/\"" >> $HOME/.bashrc
+	sudo echo -e "## Proxy settings by proxyswitch\nexport http_proxy=\"http://$proxy/\"\nexport https_proxy=\"https://$proxy/\"\nexport socks_proxy=\"socks://$proxy/\"\nexport ftp_proxy=\"ftp://$proxy/\"\nexport all_proxy=\"https://$proxy/\"" >> $HOME/.bashrc
 }
 
 # Set no proxy ... Remove all the previous proxy settings 
@@ -135,12 +138,15 @@ ProxyNone(){
 	sudo sed -i.bak '/https::proxy/d' /etc/apt/apt.conf
 	sudo sed -i.bak '/socks::proxy/d' /etc/apt/apt.conf
 	sudo sed -i.bak '/ftp::proxy/d' /etc/apt/apt.conf
+	sudo sed -i.bak '/all::proxy/d' /etc/apt/apt.conf
+	
 
 	# Environment variables set up
 	sudo sed -i.bak '/http_proxy/d' /etc/environment
 	sudo sed -i.bak '/https_proxy/d' /etc/environment
 	sudo sed -i.bak '/ftp_proxy/d' /etc/environment
 	sudo sed -i.bak '/socks_proxy/d' /etc/environment
+	sudo sed -i.bak '/all_proxy/d' /etc/environment
 
 	# exporting in .bashrc file
 	sudo sed -i.bak '/proxyswitch/d' $HOME/.bashrc
@@ -148,6 +154,7 @@ ProxyNone(){
 	sudo sed -i.bak '/https_proxy/d' $HOME/.bashrc
 	sudo sed -i.bak '/ftp_proxy/d' $HOME/.bashrc
 	sudo sed -i.bak '/socks_proxy/d' $HOME/.bashrc
+	sudo sed -i.bak '/all_proxy/d' $HOME/.bashrc
 
 	echo "All previous proxy settings removed."
 }
